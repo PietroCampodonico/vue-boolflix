a8d362c54c0872d59d448aa2d10b151dad78afba
PietroCampodonico <pietro.campodonico@outlook.it>
2021-05-13 18:19:41 +0200

    Generate query sia per ricerca film che serie tv

---
+++ script.js
@@ -0,0 +1,42 @@
+new Vue({
+    el: "#app",
+    data: {
+        contentResearch: "",
+        movieList: [],
+        tvSeriesList: [],
+    },
+
+    methods: {
+
+            
+        makeAxiosSearch(searchEntity) {
+
+            const axiosOptions = {
+                params: {
+                    api_key: "8ba1bb4e00bce0b3ac322114c49aba09",
+                    query: this.contentResearch,
+                    language: "it-IT",
+                }
+            }
+
+            axios.get("https://api.themoviedb.org/3/search/" + searchEntity, axiosOptions)
+                .then((resp) => {
+                    if (searchEntity === "movie") {
+                        this.movieList = resp.data.results
+                        console.log(this.movieList)
+
+                    } else if (searchEntity === "tv") {
+                        this.tvSeriesList = resp.data.results
+                        console.log(this.tvSeriesList)
+
+                    }
+                })
+           
+        },
+
+        onUserSearch() {
+            this.makeAxiosSearch("movie");
+            this.makeAxiosSearch("tv");
+        }
+    }
+})
